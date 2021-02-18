16b42c3c7f0e844f925618262409127ac175b5e0
tanbre <tannor@breitigam.email>
2021-02-18 09:24:11 -0800

    set up app

--- backend/app.js
+++ backend/app.js
@@ -2,5 +2,43 @@ const express = require('express');
 const morgan = require('morgan');
 const cors = require('cors');
 const csurf = require('csurf');
-const helment = require('helmet');
+const helmet = require('helmet');
 const cookieParser = require('cookie-parser');
+
+
+//  Create a variable called isProduction that will be true
+//  if the environment is in production or not by checking
+//  the environment key in the configuration file (backend/config/index.js).
+const { environment } = require('./config');
+const isProduction = environment === 'production';
+
+//  Initialize the express application
+const app = express();
+
+//  Connect the morgan middleware for logging information about requests and responses
+app.use(morgan('dev'));
+
+//  Add the cookie-parser middleware for parsing cookies
+app.use(cookieParser());
+
+//  and the express.json middleware for parsing JSON bodies of requests with Content-Type of "application/json".
+app.use(express.json());
+
+//  enable cors only in development
+if (!isProduction) app.use(cors());
+
+//  helmet helps aset a variety of headers to better secur your app
+app.use(helmet({
+    contentSecurityPolicy: false
+}));
+
+//  set the _csrf token and create req.csrfToken method
+app.use(
+    csurf({
+        cookie: {
+            secure: isProduction,
+            sameSite: isProduction && 'Lax',
+            httpOnly: true,
+        },
+    })
+);
