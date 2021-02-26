7e3968c285cc54c41eb465374095797cad519d1f
tanbre <tannor@breitigam.email>
2021-02-26 08:10:22 -0800

    carousel working better

--- frontend/src/components/PhotoCarousel/index.js
+++ frontend/src/components/PhotoCarousel/index.js
@@ -1,54 +1,18 @@
-import { useEffect, useState } from 'react';
-
 import './index.css';
 
-const PhotoCarousel = ({ photos }) => {
-
-  const [crslPhotos, setCrslPhotos] = useState(photos);
-
-  useEffect(() => {
-    setCrslPhotos([...photos]);
-  }, [])
-
-  // const rotateLeft = () => {
-  //   const reorderedPhotos = [...crslPhotos];
-  //   reorderedPhotos.unshift(reorderedPhotos.pop());
-  //   setCrslPhotos(reorderedPhotos);
-  // }
+const PhotoCarousel = ({ photos, setPhotos }) => {
 
   const rotateLeft = () => {
-    const photos = document.querySelectorAll('.photo-crsl__photo');
-    const lastIndex = photos.length - 1;
-    console.log('   :::LASTINDEX:::   ', lastIndex);
-    const lastImageUrl = photos[lastIndex].style.backgroundImage;
-    for (let i = lastIndex; i >= 0; i--) {
-      if (i === 0) {
-        photos[i].style.backgroundImage = lastImageUrl;
-      } else {
-        photos[i].style.backgroundImage = photos[i - 1].style.backgroundImage;
-      }
-
-    }
+    const reorderedPhotos = [...photos];
+    reorderedPhotos.unshift(reorderedPhotos.pop());
+    setPhotos(reorderedPhotos);
   }
 
   const rotateRight = () => {
-    const photos = document.querySelectorAll('.photo-crsl__photo');
-    const firstImageUrl = photos[0].style.backgroundImage;
-    for (let i = 0; i < photos.length; i++) {
-      if (i === photos.length - 1) {
-        photos[i].style.backgroundImage = firstImageUrl;
-      } else {
-        photos[i].style.backgroundImage = photos[i + 1].style.backgroundImage;
-      }
-
-    }
+    const reorderedPhotos = [...photos];
+    reorderedPhotos.push(reorderedPhotos.shift());
+    setPhotos(reorderedPhotos);
   }
-
-  // const rotateRight = () => {
-  //   const reorderedPhotos = [...crslPhotos];
-  //   reorderedPhotos.push(reorderedPhotos.shift());
-  //   setCrslPhotos(reorderedPhotos);
-  // }
 
   return (
     <div className='photo-crsl'>
--- frontend/src/components/Spot/index.js
+++ frontend/src/components/Spot/index.js
@@ -1,4 +1,4 @@
-import { useEffect } from 'react';
+import { useEffect,useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { useParams } from 'react-router-dom';
 
@@ -10,17 +10,22 @@ const Spot = () => {
   const dispatch = useDispatch();
   const { id } = useParams();
 
+  const [photos, setPhotos] = useState([]);
+
   useEffect(() => {
     dispatch(getSpot(id))
+      .then((spot) => setPhotos(spot.Photos))
   }, [])
 
   const spot = useSelector(state => state.spots.spot);
+
+  // setPhotos(spot?.Photos)
 
   return (
     <div className='spot'>
       {
         spot &&
-        <PhotoCarousel photos={spot.Photos} />
+        <PhotoCarousel photos={photos} setPhotos={setPhotos} />
       }
       <p>{JSON.stringify(spot)}</p>
     </div>
